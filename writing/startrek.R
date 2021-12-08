08544c2c8e5fc73ef79ec5767743261a6e8f1dc5
Stacey Elmore <elmore.stacey@gmail.com>
2021-12-07 23:53:46 -0700

    Rename word column in startrek_tidy to eliminate confusion with sentiment "bing" words

--- writing/startrek.R
+++ writing/startrek.R
@@ -2,26 +2,42 @@
 library(tidyverse)
 library(tidytext)
 library(textdata)
+library(magrittr)
+
 
 startrek <- readr::read_csv(
   "https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-08-17/computer.csv")
 
+data(stop_words)
+
 #tidy the data
-startrek_tidy <- startrek %>% 
+startrek_tidy <- tibble(startrek)  
+startrek_tidy <- startrek_tidy %>% 
   unnest_tokens(words, line) %>% 
-  unlist(words)
-  #anti_join(stop_words)
+  rename("script_words" = words)
+unlist(script_words)
+
+
+#get sentiments from bing dataset (pos/neg)
+sentiment <- get_sentiments("bing") 
+sentiment <- sentiment %>% 
+  filter(words %in% words)
+
 
 ######Sentiment Analysis######
 #sent_afinn <- get_sentiments("afinn")
 #sent_nrc <- get_sentiments("nrc")
 #sent_loughran <- get_sentiments("loughran")
 
-#get sentiments from bing dataset (pos/neg)
-sentiment <- unlist(get_sentiments("bing")) 
- 
-star_sent <- startrek_tidy %>% 
-  inner_join(sentiment, startrek_tidy, by = c("words" = "words"), copy = TRUE) 
+
+select(words, w1, new_word) %>% 
+  filter(w1 %in% wordy$words) %>% 
+  filter(new_word %in% wordy$words)
+
+
+startrek_sent <- startrek_tidy
+startrek_sent %>% 
+  inner_join(sentiment, startrek_tidy, copy = TRUE, suffix = c(".sentiment", ".startrek_tidy")) 
     count(word, sort = TRUE) %>% 
     pivot_wider(names_from = , values_from = n, values_fill = 0) %>% 
     mutate(sentiment = positive - negative)
