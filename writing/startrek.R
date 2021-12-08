9b38577ad51a524b31f0ee5d75947ecb4f6560ea
Stacey Elmore <elmore.stacey@gmail.com>
2021-12-08 02:58:25 -0700

    Got Bing sentiment analysis to run, updating code

--- writing/startrek.R
+++ writing/startrek.R
@@ -4,49 +4,58 @@ library(tidytext)
 library(textdata)
 library(magrittr)
 
-
 startrek <- readr::read_csv(
   "https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-08-17/computer.csv")
 
 data(stop_words)
-
 #tidy the data
-startrek_tidy <- tibble(startrek)  
+startrek_tidy <- tibble(startrek)
 startrek_tidy <- startrek_tidy %>% 
+  select(char, line) %>% 
   unnest_tokens(words, line) %>% 
-  rename("script_words" = words)
-unlist(script_words)
+  rename("script_words" = words) 
+  
+sent_afinn <- get_sentiments("afinn") 
+
+sent_nrc <- get_sentiments("nrc") %>% 
+  filter(word %in% startrek_tidy$script_words)
+
+sent_loughran <- get_sentiments("loughran") %>% 
+  filter(word %in% startrek_tidy$script_words)
+
+sentiment_bing <- get_sentiments("bing") %>% 
+  filter(word %in% startrek_tidy$script_words)
+
+######Sentiment Analysis (pos/neg)######
+
+startrek_sentiment <- sentiment_bing %>% 
+  inner_join(sentiment_bing, copy = TRUE) %>% 
+  add_column(obs = 1:244)
+
+count_sent <- startrek_sentiment %>% 
+  count(sentiment) %>% 
+  pivot_wider(names_from = sentiment, values_from = n, values_fill = 0) %>% 
+  mutate(difference = positive - negative) 
+  
+p <- startrek_sentiment %>% 
+  ggplot(aes(sentiment, obs)) +
+  geom_col(show.legend = FALSE) 
+  #facet_wrap(~obs, nrow = 2, ncol = 244 , scales = "fixed")
+print(p)
 
 
-#get sentiments from bing dataset (pos/neg)
-sentiment <- get_sentiments("bing") 
-sentiment <- sentiment %>% 
-  filter(words %in% words)
-
-
-######Sentiment Analysis######
-#sent_afinn <- get_sentiments("afinn")
-#sent_nrc <- get_sentiments("nrc")
-#sent_loughran <- get_sentiments("loughran")
-
-
-select(words, w1, new_word) %>% 
-  filter(w1 %in% wordy$words) %>% 
-  filter(new_word %in% wordy$words)
-
-
-startrek_sent <- startrek_tidy
-startrek_sent %>% 
-  inner_join(sentiment, startrek_tidy, copy = TRUE, suffix = c(".sentiment", ".startrek_tidy")) 
-    count(word, sort = TRUE) %>% 
-    pivot_wider(names_from = , values_from = n, values_fill = 0) %>% 
-    mutate(sentiment = positive - negative)
-
-
-ggplot(startrek_sent, aes(index, sentiment, fill = line)) +
-  geom_col(show.legend = FALSE) +
-  facet_wrap(~line, ncol = 2, scales = "free_x")
-
-
-
-
+##Comparison cloud
+# x <- austin_tidy %>%
+#   anti_join(bind_rows(stop_words,
+#                       tibble(word = c("miss"), lexicon = c("custom")))) %>%
+#   inner_join(get_sentiments("bing")) %>% 
+#   filter(book == "Pride & Prejudice") %>%
+#   count(word, sentiment, sort = TRUE) %>%
+#   pivot_wider(names_from = sentiment, values_from = n, values_fill = list(n = 0)) %>%
+#   as.data.frame()
+# ## Joining, by = "word"
+# ## Joining, by = "word"
+# rownames(x) <- x[,1]
+# comparison.cloud(x[, 2:3])
+# ## Warning in comparison.cloud(x[, 2:3]): agreeable could not be fit on page. It
+# ## will not be plotted.
