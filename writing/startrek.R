2c2e24717ea26520c7a0e4727049d09ab7c5e463
Stacey Elmore <elmore.stacey@gmail.com>
2021-12-13 23:31:31 -0700

    Upload finished code for Final Project

--- writing/startrek.R
+++ writing/startrek.R
@@ -2,8 +2,6 @@
 library(tidyverse)
 library(tidytext)
 library(textdata)
-library(reshape2)
-library(RColorBrewer)
 library(viridis)
 
 startrek <- readr::read_csv(
@@ -24,44 +22,37 @@ startrek_tidy2 <- startrek %>%
                        'Picard (O.S.)' = "Picard", 'Picard (V.O.)' = "Picard", 'Young Picard' = "Picard",
                        'Jean-Luc' = "Picard")) 
 
-select_char <- c("Picard", "Geordi", "Data", "Riker", "Computer")
+select_char <- c("Picard", "Geordi", "Data", "Riker", "Computer", "Beverly")
 startrek_tidy3 <- filter(startrek_tidy2, char %in% select_char)
 
 ######Sentiment Analysis######
 
 ##bing
-sentiment_bing <- get_sentiments("bing")  #pos/neg
-
-bing_join <- sentiment_bing %>% 
+bing_join <- get_sentiments("bing") %>% 
   inner_join(startrek_tidy3, by = c("word" = "script_words")) %>% 
   group_by(sentiment, char) %>% 
   count()
 
-bing_summary <- bing_join %>% 
-  pivot_wider(names_from = sentiment, values_from = n, values_fill = 0) %>% 
-  mutate(difference = positive - negative) 
-
 plot_bing <- bing_join %>% 
     ggplot(aes(char, sentiment, fill = sentiment, y = n)) +
     geom_bar(position = "stack", stat = "identity") +
-    xlab("Star Trek Character") +
-    ylab("Number of words") +
+    xlab("Star Trek Character") + ylab("Number of words") +
     labs(title = "TNG Character and Computer Interactions", 
          subtitle = "Word Sentiment Analysis of the 'Bing' Lexicon", 
          caption = "Data source: www.tidytuesday.com") +
     theme(
       plot.title = element_text(size = 14, hjust = 0.5),
       plot.subtitle = element_text(size = 11, hjust = 0.5),
-      plot.caption = element_text(size = 9, hjust = 0.9),
+      plot.caption = element_text(size = 9, hjust = 0.5),
       legend.title = element_blank(),
       panel.grid.minor.x=element_blank(),
       panel.grid.major.x=element_blank(),
       axis.text.x = element_text(angle = 60, vjust = 0.5, hjust = 0.5),
       ) 
-  print(plot_bing)
+print(plot_bing)
 
 ###nrc (feelings)
-omit_sent <- c("positive", "negative")
+omit_sent <- c("positive", "negative") #take out data with pos/neg because already analyzed
 sent_nrc <- get_sentiments("nrc") %>% 
   filter(!sentiment %in% omit_sent)
   
@@ -73,15 +64,14 @@ nrc_join <- sent_nrc %>%
 plot_nrc <- nrc_join %>% 
   ggplot(aes(char, sentiment, fill = sentiment, y = n)) +
   geom_bar(position = "stack", stat = "identity") +
-  xlab("Star Trek Character") +
-  ylab("Number of words") +
+  xlab("Star Trek Character") + ylab("Number of words") +
   labs(title = "TNG Character and Computer Interactions", 
        subtitle = "Word Sentiment Analysis of the 'NRC' Lexicon", 
        caption = "Data source: www.tidytuesday.com") +
   theme(
     plot.title = element_text(size = 14, hjust = 0.5),
     plot.subtitle = element_text(size = 11, hjust = 0.5),
-    plot.caption = element_text(size = 9, hjust = 0.9),
+    plot.caption = element_text(size = 8, hjust = 0.5),
     legend.title = element_blank(),
     panel.grid.minor.x=element_blank(),
     panel.grid.major.x=element_blank(),
@@ -91,28 +81,34 @@ print(plot_nrc)
 
 ###afinn (score)
 sent_afinn <- get_sentiments("afinn") #score
-
 afinn_join <- sent_afinn %>% 
   inner_join(startrek_tidy3, by = c("word" = "script_words")) %>% 
   group_by(value, char) %>% 
   count() 
 
+x_labels = c(-4:4)
 plot_afinn <- afinn_join %>% 
-  ggplot(aes(char, n, fill = value)) +
+  ggplot(aes(value, n, fill = value)) +
   geom_bar(position = "dodge", stat = "identity") +
-  xlab("Character") +
-  ylab("Sentiment Score") +
+  scale_fill_viridis(option = "D") +
+  scale_color_viridis(option = "D") +
+  facet_wrap(vars(char)) +
+  xlab("Sentiment Score") +
+  ylab("Word Count") +
   labs(title = "TNG Character and Computer Interactions", 
        subtitle = "Word Sentiment Analysis of the 'Afinn' Lexicon", 
-       caption = "Data source: www.tidytuesday.com") +
+       caption = "Data source: www.tidytuesday.com", 
+       color = "Word Count") +
   theme(
     plot.title = element_text(size = 14, hjust = 0.5),
     plot.subtitle = element_text(size = 11, hjust = 0.5),
-    plot.caption = element_text(size = 9, hjust = 0.9),
-    legend.title = element_blank(),
+    plot.caption = element_text(size = 9, hjust = 0.5),
     panel.grid.minor.x=element_blank(),
     panel.grid.major.x=element_blank(),
-    axis.text.x = element_text(angle = 60, vjust = 0.5, hjust = 0.5),
-  ) 
+    panel.grid.minor.y=element_blank(),
+    panel.grid.major.y=element_blank()) +
+  theme(legend.position = "bottom", legend.title = element_blank()) +
+  scale_x_continuous(labels = x_labels, breaks = x_labels)
+
 print(plot_afinn)
 
